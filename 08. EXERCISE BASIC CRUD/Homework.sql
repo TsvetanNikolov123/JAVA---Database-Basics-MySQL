ffa84b5ffc254b09923a88fb3d5e63e7da128022
TsvetanNikolov123 <tsdman1985@gmail.com>
2018-02-04 22:55:53 +0200

    finished few more exercises

--- 08. EXERCISE BASIC CRUD/Homework.sql
+++ 08. EXERCISE BASIC CRUD/Homework.sql
@@ -1,4 +1,55 @@
+-- ---1---
+SELECT * FROM `departments`;
+
+-- ---2---
 SELECT 
-    first_name, last_name, salary
+    `name`
 FROM
-    employees;
+    `departments`;
+
+-- ---3---
+SELECT 
+    `first_name`, `last_name`, `salary`
+FROM
+    `employees`;
+    
+-- ---4---
+SELECT 
+    `first_name`, `middle_name`, `last_name`
+FROM
+    `employees`;
+
+-- ---5---
+SELECT 
+    CONCAT(`first_name`,
+            '.',
+            `last_name`,
+            '@softuni.bg') AS 'full_ email_address'
+FROM
+    `employees`;
+
+-- ---6---
+SELECT DISTINCT
+    `salary`
+FROM
+    `employees`
+ORDER BY `salary` ASC;
+
+-- ---7---
+SELECT 
+    *
+FROM
+    `employees`
+WHERE
+    `job_title` = 'Sales Representative';
+    
+-- ---8---
+SELECT 
+    `first_name`, `last_name`, `job_title`
+FROM
+    `employees`
+WHERE
+    `salary` BETWEEN 20000 AND 30000;
+
+-- ---9---
+
