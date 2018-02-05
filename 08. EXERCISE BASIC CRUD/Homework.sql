9ced6037bfebde1670481a3434267b55adb44819
TsvetanNikolov123 <tsdman1985@gmail.com>
2018-02-05 17:27:42 +0200

    homework finished.

--- 08. EXERCISE BASIC CRUD/Homework.sql
+++ 08. EXERCISE BASIC CRUD/Homework.sql
@@ -92,7 +92,106 @@ WHERE
     NOT `department_id` = 4;
     
 -- ---14---
+SELECT 
+    *
+FROM
+    `employees`
+ORDER BY 	`salary` DESC ,
+			`first_name` ASC ,
+            `last_name` DESC ,
+            `middle_name` ASC;
+            
+-- ---15---
+CREATE VIEW `v_employees_salaries` AS
+    SELECT 
+        `first_name`, `last_name`, `salary`
+    FROM
+        `employees`;
 
-	
+-- ---16---
+UPDATE `employees` 
+SET 
+    `middle_name` = ''
+WHERE
+    `middle_name` IS NULL;
+CREATE VIEW `v_employees_job_titles` AS
+    SELECT 
+        CONCAT(`first_name`,
+                ' ',
+                `middle_name`,
+                ' ',
+                `last_name`) AS 'full_name',
+        `job_title`
+    FROM
+        `employees`;
+        
+-- ---17---
+SELECT DISTINCT
+    `job_title`
+FROM
+    `employees`
+ORDER BY `job_title` ASC; 
 
+-- ---18---
+SELECT 
+    *
+FROM
+    `projects`
+ORDER BY `start_date` ASC , `name` ASC , `project_id` ASC
+LIMIT 10;
+
+-- ---19---
+SELECT 
+    `first_name`, `last_name`, `hire_date`
+FROM
+    `employees`
+ORDER BY `hire_date` DESC
+LIMIT 7;
+
+-- ---20---
+UPDATE `employees` 
+SET 
+    `salary` = `salary` * 1.12
+WHERE
+    `department_id` = '1'
+        OR `department_id` = '2'
+        OR `department_id` = '4'
+        OR `department_id` = '11';
+
+SELECT `salary` FROM `employees`;
+
+-- ---21---
+SELECT 
+    `peak_name`
+FROM
+    `peaks`
+ORDER BY `peak_name` ASC;
+
+-- ---22---
+SELECT 
+    `country_name`, `population`
+FROM
+    `countries`
+WHERE
+    `continent_code` = 'EU'
+ORDER BY `population` DESC , `country_name` ASC
+LIMIT 30;
+
+-- ---23---
+SELECT 
+    `country_name`,
+    `country_code`,
+    IF(`currency_code` = 'EUR',
+        'Euro',
+        'Not Euro') AS `currency`
+FROM
+    `countries`
+ORDER BY `country_name` ASC;
+
+-- ---24---
+SELECT 
+    `name`
+FROM
+    `characters`
+ORDER BY `name` ASC;
 
