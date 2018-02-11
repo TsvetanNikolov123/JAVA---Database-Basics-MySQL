6b92a5981a4314a7e3fb77b5e689ad69bbd3b09b
TsvetanNikolov123 <tsdman1985@gmail.com>
2018-02-11 16:44:45 +0200

    .

--- 12. EXERCISE DATA AGGREGATION/homework.sql
+++ 12. EXERCISE DATA AGGREGATION/homework.sql
@@ -33,3 +33,49 @@ ORDER BY AVG(`w`.`magic_wand_size`)
 LIMIT 1;
 
 -- ---5---
+
+SELECT 
+    `w`.`deposit_group`,
+    SUM(`w`.`deposit_amount`) AS 'total_sum'
+FROM
+    `wizzard_deposits` AS `w`
+GROUP BY `w`.`deposit_group`
+ORDER BY `total_sum`;
+
+-- ---6---
+
+SELECT 
+    `w`.`deposit_group`,
+    SUM(`w`.`deposit_amount`) AS 'total_sum'
+FROM
+    `wizzard_deposits` AS `w`
+WHERE
+    `w`.`magic_wand_creator` = 'Ollivander family'
+GROUP BY `w`.`deposit_group`;
+
+-- ---7---
+
+SELECT 
+    `w`.`deposit_group`,
+    SUM(`w`.`deposit_amount`) AS 'total_sum'
+FROM
+    `wizzard_deposits` AS `w`
+WHERE
+    `w`.`magic_wand_creator` = 'Ollivander family'
+GROUP BY `w`.`deposit_group`
+HAVING `total_sum` < 150000
+ORDER BY `total_sum` DESC;
+
+-- ---8---
+
+SELECT 
+    `w`.`deposit_group`,
+    `w`.`magic_wand_creator`,
+    MIN(`w`.`deposit_charge`)
+FROM
+    `wizzard_deposits` AS `w`
+GROUP BY `w`.`deposit_group` , `w`.`magic_wand_creator`
+ORDER BY `w`.`magic_wand_creator` ASC , `w`.`deposit_group` ASC;
+
+-- --9---
+
