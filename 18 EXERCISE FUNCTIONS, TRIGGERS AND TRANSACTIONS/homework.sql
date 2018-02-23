7de8674ff1d441c11a9d861cf59a8019cd651a8a
TsvetanNikolov123 <tsdman1985@gmail.com>
2018-02-23 23:33:47 +0200

    one more task finished

--- 18 EXERCISE FUNCTIONS, TRIGGERS AND TRANSACTIONS/homework.sql
+++ 18 EXERCISE FUNCTIONS, TRIGGERS AND TRANSACTIONS/homework.sql
@@ -48,8 +48,42 @@ WHERE
     salary >= number
 ORDER BY e.first_name ASC , e.last_name ASC , e.employee_id ASC;
 END $$
-DELIMITER $$
+DELIMITER ;
 
 -- -------------------------------------------------------------------
 -- 3.	Town Names Starting With
 -- -------------------------------------------------------------------
+/*
+	Write a stored procedure usp_get_towns_starting_with that accept 
+    string as parameter and returns all town names starting with that 
+    string. The result should be sorted by town_name alphabetically. 
+    Submit your query statement as Run skeleton, run queries & check DB 
+    in Judge.
+*/
+DELIMITER $$
+CREATE PROCEDURE usp_get_towns_starting_with(some_string VARCHAR(255))
+BEGIN
+	SELECT 
+    t.name AS town_name
+FROM
+    towns AS t
+WHERE
+    t.name LIKE CONCAT(some_string, '%')
+ORDER BY town_name ASC;
+END $$
+DELIMITER ;
+
+-- -------------------------------------------------------------------
+-- 4.	Employees from Town
+-- -------------------------------------------------------------------
+/*
+	Write a stored procedure usp_get_employees_from_town that accepts
+    town_name as parameter and return the employees’ first and last 
+    name that live in the given town. The result should be sorted by 
+    first_name then by last_name alphabetically and id ascending. 
+    Submit your query statement as Run skeleton, run queries & check 
+    DB in Judge.
+*/
+
+
+
